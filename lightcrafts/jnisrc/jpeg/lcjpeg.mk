f5fe9ed01bc66dfdd378cb9f71e191fb8254cb13
KITAGAWA Masahiro <arctica0316@gmail.com>
2014-04-09 22:36:24 +0900

    Added static libs for Windows from Cygwin Ports; updated .gitignore

--- lightcrafts/jnisrc/jpeg/lcjpeg.mk
+++ lightcrafts/jnisrc/jpeg/lcjpeg.mk
@@ -1,3 +1,7 @@
+ROOT:=		../../..
+COMMON_DIR:=	$(ROOT)/lightcrafts
+include		$(COMMON_DIR)/mk/platform.mk
+
 HIGH_PERFORMANCE:=	1
 
 TARGET_BASE:=		LCJPEG
@@ -6,12 +10,14 @@ TARGET_BASE:=		LCJPEG
 #DEBUG:=		true
 
 JNI_EXTRA_CFLAGS:=	-fexceptions
+ifeq ($(PLATFORM),Windows)
+  JNI_EXTRA_LDFLAGS:=   -Larch-$(PROCESSOR)
+endif
 JNI_EXTRA_LINK:=	-ljpeg -lLCJNI
 
 JAVAH_CLASSES:=		com.lightcrafts.image.libs.LCJPEGReader \
 			com.lightcrafts.image.libs.LCJPEGWriter
 
-ROOT:=			../../..
 include			../jni.mk
 
 # vim:set noet sw=8 ts=8:
--- lightcrafts/jnisrc/tiff/lctiff.mk
+++ lightcrafts/jnisrc/tiff/lctiff.mk
@@ -1,6 +1,6 @@
-ROOT:=			../../..
-COMMON_DIR:=		$(ROOT)/lightcrafts
-include			$(COMMON_DIR)/mk/platform0.mk
+ROOT:=		../../..
+COMMON_DIR:=	$(ROOT)/lightcrafts
+include		$(COMMON_DIR)/mk/platform.mk
 
 HIGH_PERFORMANCE:=	1
 ifeq ($(PLATFORM),MacOSX)
@@ -13,10 +13,13 @@ TARGET_BASE:=		LCTIFF
 #DEBUG:=		true
 
 ifeq ($(PLATFORM),Windows)
-  JNI_EXTRA_LINK:=	-Wl,-Bdynamic -lLCJNI -Wl,-Bstatic -ltiff -lstdc++ 
+  JNI_EXTRA_LDFLAGS:=   -Larch-$(PROCESSOR) -L../jpeg/arch-$(PROCESSOR)
+  JNI_EXTRA_LINK:=	-Wl,-Bdynamic -lLCJNI -Wl,-Bstatic -ltiff -ljpeg -ljbig -llzma -lz -lstdc++ 
 else
   JNI_EXTRA_LINK:=	-lLCJNI -ltiff -lstdc++ 
 endif
+
+JNI_EXTRA_DISTCLEAN:=	autom4te.cache configure config.* tif_config.h
 
 JAVAH_CLASSES:=		com.lightcrafts.image.libs.LCTIFFCommon \
 			com.lightcrafts.image.libs.LCTIFFReader \
