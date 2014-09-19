20a5c9078d23049fb6f7536c8fb8ff92ada0c1c9
KITAGAWA Masahiro <arctica0316@gmail.com>
2014-09-19 20:41:49 +0900

    Amend to last commit

---
+++ lightcrafts/src/com/lightcrafts/model/ImageEditor/Locale.java
@@ -0,0 +1,12 @@
+/* Copyright (C) 2014 Masahiro Kitagawa */
+
+package com.lightcrafts.model.ImageEditor;
+
+import com.lightcrafts.utils.LocaleUtil;
+
+final class Locale {
+
+    static final LocaleUtil LOCALE = new LocaleUtil( Locale.class );
+
+}
+/* vim:set et sw=4 ts=4: */
