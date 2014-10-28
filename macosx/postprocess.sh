872dca656de413e6416c0f811197255d48838957
KITAGAWA Masahiro <arctica0316@gmail.com>
2014-10-28 23:26:03 +0900

    MacOSX - Update .dylib for x86_64; add postprocess.sh

---
+++ macosx/postprocess.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Remove dylib path that depends on a build environment
+# from libraries under specified directory.
+
+if [ $# -ne 1 ]; then
+  echo "Usage: "`basename $0`" DIRECTORY" 1>&2
+  exit 1
+fi
+
+PATTERN="/usr/local/"
+
+cd $1
+for lib in *.dylib *.jnilib `find . -perm +0111 -type f`; do
+  otool -L ${lib} | grep ${PATTERN} | while read line; do
+    fullpath=`echo ${line} | sed -e "s% *\(${PATTERN}[^ ]*\.dylib\).*%\1%"`
+    name=`echo ${fullpath} | sed -e "s%.*/\(.*\.dylib\)%\1%"`
+    install_name_tool -change ${fullpath} ${name} ${lib}
+  done
+done
