36fb87586877baecf9aae243e19cbcdeaa2763f2
KITAGAWA Masahiro <arctica0316@gmail.com>
2014-12-13 16:12:26 +0900

    Linux - Fix I/O exception in jhindexer

--- linux/help/jhindexer.sh
+++ linux/help/jhindexer.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 echo jhindexer $@
 cd $@
-java -cp "/usr/share/java/javahelp/*:/usr/share/java/*:/usr/share/javahelp/lib/*:/usr/local/share/java/classes/*:/usr/jdk/packages/lib/ext/*" com.sun.java.help.search.Indexer .
+java -cp "/usr/share/java/javahelp/*:/usr/share/java/*:/usr/share/javahelp/lib/*:/usr/local/share/java/classes/*:/usr/jdk/packages/lib/ext/*" com.sun.java.help.search.Indexer ./*.html
