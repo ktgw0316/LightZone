ktgw0316/LightZone

commit 704f706f76cbbd5e532de43c8b1049ca1dd9cd33

Merge branch master into testing

lightcrafts/src/com/lightcrafts/jai/opimage/ShapedMask.java
@@ -1,5 +1,5 @@
 /* Copyright (C) 2005-2011 Fabio Riccardi */
-/* Copyright (C) 2015 Masahiro Kitagawa */
+/* Copyright (C) 2015-     Masahiro Kitagawa */
 
 package com.lightcrafts.jai.opimage;
 
@@ -27,12 +27,12 @@ import java.util.*;
  * To change this template use File | Settings | File Templates.
  */
 public class ShapedMask extends PlanarImage {
-    Region region;
-    LCROIShape shape;
+    private Region region;
+    private LCROIShape shape;
 
     public static Rectangle getOuterBounds(Region region, AffineTransform transform) {
         Rectangle outerBounds = null;
-        for (Contour c : region.getContours()) {
+        for (final Contour c : region.getContours()) {
             AffineTransform combined = transform;
             if (c.getTranslation() != null) {
                 combined = AffineTransform.getTranslateInstance(c.getTranslation().getX(), c.getTranslation().getY());
@@ -51,28 +51,24 @@ public class ShapedMask extends PlanarImage {
         return outerBounds;
     }
 
-    static ImageLayout createLayout(Region region, AffineTransform transform) {
+    private static ImageLayout createLayout(Region region, AffineTransform transform) {
         Rectangle regionBounds = getOuterBounds(region, transform);
 
         SampleModel graySm = RasterFactory.createPixelInterleavedSampleModel(DataBuffer.TYPE_BYTE, regionBounds.width, regionBounds.height, 1);
         ColorModel grayCm = new ComponentColorModel(ColorSpace.getInstance(ColorSpace.CS_GRAY), false, false, Transparency.OPAQUE, DataBuffer.TYPE_BYTE);
 
-        ImageLayout layout = new ImageLayout(regionBounds.x, regionBounds.y, regionBounds.width, regionBounds.height,
-                                             regionBounds.x, regionBounds.y, JAIContext.TILE_WIDTH, JAIContext.TILE_HEIGHT,
-                                             graySm, grayCm);
-
-        return layout;
+        return new ImageLayout(regionBounds.x, regionBounds.y, regionBounds.width, regionBounds.height,
+                               regionBounds.x, regionBounds.y, JAIContext.TILE_WIDTH, JAIContext.TILE_HEIGHT,
+                               graySm, grayCm);
     }
 
     static ImageLayout createLayout(Rectangle regionBounds) {
         SampleModel graySm = RasterFactory.createPixelInterleavedSampleModel(DataBuffer.TYPE_BYTE, regionBounds.width, regionBounds.height, 1);
         ColorModel grayCm = new ComponentColorModel(ColorSpace.getInstance(ColorSpace.CS_GRAY), false, false, Transparency.OPAQUE, DataBuffer.TYPE_BYTE);
 
-        ImageLayout layout = new ImageLayout(regionBounds.x, regionBounds.y, regionBounds.width, regionBounds.height,
-                                             regionBounds.x, regionBounds.y, JAIContext.TILE_WIDTH, JAIContext.TILE_HEIGHT,
-                                             graySm, grayCm);
-
-        return layout;
+        return new ImageLayout(regionBounds.x, regionBounds.y, regionBounds.width, regionBounds.height,
+                               regionBounds.x, regionBounds.y, JAIContext.TILE_WIDTH, JAIContext.TILE_HEIGHT,
+                               graySm, grayCm);
     }
 
     public ShapedMask(Region region, LCROIShape shape) {
@@ -97,7 +93,7 @@ public class ShapedMask extends PlanarImage {
         int size = blurs.size();
         Shape[] result = new Shape[size];
         int i = 0;
-        for (Shape b : blurs)
+        for (final Shape b : blurs)
             result[size - 1 - i++] = b;
         return result;
     }
@@ -117,6 +113,9 @@ public class ShapedMask extends PlanarImage {
         g2d.setColor(Color.white);
         g2d.fill(shape);
 
+        if (contourWidth <= 1)
+            return;
+
         // Draw the blurs in shades of gray:
         if (contourWidth > 1) {
             int width = (int) Math.floor(contourWidth);
@@ -134,7 +133,6 @@ public class ShapedMask extends PlanarImage {
                 g2d.fill(semiBlur);
             }
         }
-
     }
 
     private static ScaledImage createContourImage(Contour contour) {
@@ -195,11 +193,11 @@ public class ShapedMask extends PlanarImage {
             RenderingHints hints = new RenderingHints(JAI.KEY_BORDER_EXTENDER, extender);
 
             contourImage.image = JAI.create("LCSeparableConvolve", pb, hints);
-        } else
+        } else {
             contourImage.image = (PlanarImage) supportImage;
+        }
 
         contourImage.scale = widthScale;
-
         contourImage.tx = bounds.x;
         contourImage.ty = bounds.y;
 
@@ -236,12 +234,13 @@ public class ShapedMask extends PlanarImage {
             raster = r;
         }
 
+        @Override
         public Raster getTile(int tileX, int tileY) {
             return raster;
         }
     }
 
-    private static Map<AffinedImage, PlanarImage> expandedMasks =
+    private static final Map<AffinedImage, PlanarImage> expandedMasks =
             new SoftValueHashMap<AffinedImage, PlanarImage>();
 
     private static class AffinedImage {
@@ -253,18 +252,23 @@ public class ShapedMask extends PlanarImage {
             this.transform = transform;
         }
 
+        @Override
         public boolean equals(Object o) {
             if (this == o) return true;
-            if (!(o instanceof AffinedImage)) return false;
+            if (!(o instanceof AffinedImage))
+                return false;
 
             final AffinedImage affinedImage = (AffinedImage) o;
 
-            if (image != null ? !image.equals(affinedImage.image) : affinedImage.image != null) return false;
-            if (transform != null ? !transform.equals(affinedImage.transform) : affinedImage.transform != null) return false;
+            if (image != null ? !image.equals(affinedImage.image) : affinedImage.image != null)
+                return false;
+            if (transform != null ? !transform.equals(affinedImage.transform) : affinedImage.transform != null)
+                return false;
 
             return true;
         }
 
+        @Override
         public int hashCode() {
             int result;
             result = (image != null ? image.hashCode() : 0);
@@ -273,6 +277,7 @@ public class ShapedMask extends PlanarImage {
         }
     }
 
+    @Override
     public Raster getData(Rectangle rect) {
         // SampleModel sampleModel = RasterFactory.createPixelInterleavedSampleModel(DataBuffer.TYPE_BYTE, rect.width, rect.height, 1);
         ColorModel colorModel = new ComponentColorModel(ColorSpace.getInstance(ColorSpace.CS_GRAY),
@@ -287,7 +292,7 @@ public class ShapedMask extends PlanarImage {
 
         boolean overlay = false;
 
-        for (Contour c : region.getContours()) {
+        for (final Contour c : region.getContours()) {
             AffineTransform combined = shape.getTransform();
             if (c.getTranslation() != null) {
                 combined = AffineTransform.getTranslateInstance(c.getTranslation().getX(),
@@ -299,19 +304,18 @@ public class ShapedMask extends PlanarImage {
             Rectangle bounds = c.getOuterShape().getBounds();
             bounds.grow((int) c.getWidth(), (int) c.getWidth());
 
-            if (combined.createTransformedShape(bounds).intersects(rect)) {
-                ScaledImage scaledImage = getContourImage(c);
-                PlanarImage maskImage = scaledImage.image;
+            if (!combined.createTransformedShape(bounds).intersects(rect))
+                continue;
 
-                if (!combined.isIdentity() || scaledImage.scale < 1 || scaledImage.tx != 0 || scaledImage.ty != 0) {
-                    AffineTransform transform = new AffineTransform(combined);
+            ScaledImage scaledImage = getContourImage(c);
+            PlanarImage maskImage = scaledImage.image;
 
-                    if (scaledImage.scale < 1) {
-                        float scaleX = (float) Math.floor(maskImage.getWidth() / scaledImage.scale) / (float) maskImage.getWidth();
-                        float scaleY = (float) Math.floor(maskImage.getHeight() / scaledImage.scale) / (float) maskImage.getHeight();
+            if (!combined.isIdentity() || scaledImage.scale < 1 || scaledImage.tx != 0 || scaledImage.ty != 0) {
+                AffineTransform transform = new AffineTransform(combined);
 
-                        transform.concatenate(AffineTransform.getScaleInstance(scaleX, scaleY));
-                    }
+                if (scaledImage.scale < 1) {
+                    float scaleX = (float) Math.floor(maskImage.getWidth() / scaledImage.scale) / (float) maskImage.getWidth();
+                    float scaleY = (float) Math.floor(maskImage.getHeight() / scaledImage.scale) / (float) maskImage.getHeight();
 
                     if (scaledImage.tx != 0 || scaledImage.ty != 0)
                         transform.concatenate(AffineTransform.getTranslateInstance(scaledImage.tx, scaledImage.ty));
@@ -342,38 +346,68 @@ public class ShapedMask extends PlanarImage {
                     }
                 }
 
-                if (maskImage.getBounds().intersects(rect)) {
-                    Rectangle itx = maskImage.getBounds().intersection(rect);
+                if (scaledImage.tx != 0 || scaledImage.ty != 0)
+                    transform.concatenate(AffineTransform.getTranslateInstance(scaledImage.tx, scaledImage.ty));
 
-                    byte resultData[];
-                    if (!overlay) {
-                        resultData = (byte[]) maskImage.getData(itx).getDataElements(itx.x, itx.y, itx.width, itx.height, null);
-                        overlay = true;
+                Rectangle scaledBounds = transform.createTransformedShape(maskImage.getBounds()).getBounds();
+
+                // Avoid scaling underflows resulting into exeptions
+                if (scaledBounds.width < 3 || scaledBounds.height < 3)
+                    continue;
+
+                synchronized (expandedMasks) {
+                    AffinedImage key = new AffinedImage(maskImage, transform);
+                    PlanarImage affinedImage = expandedMasks.get(key);
+                    if (affinedImage == null) {
+                        RenderingHints hints = new RenderingHints(JAI.KEY_BORDER_EXTENDER,
+                                BorderExtender.createInstance(BorderExtender.BORDER_COPY));
+                        // hints.add(JAIContext.noCacheHint);
+                        Interpolation interp = Interpolation.getInstance(Interpolation.INTERP_BILINEAR);
+                        ParameterBlock params = new ParameterBlock();
+                        params.addSource(maskImage);
+                        params.add(transform);
+                        params.add(interp);
+                        maskImage = JAI.create("Affine", params, hints);
+                        expandedMasks.put(key, maskImage);
                     } else {
-                        resultData = (byte[]) result.getDataElements(itx.x, itx.y, itx.width, itx.height, null);
-                        byte currentData[] = (byte[]) maskImage.getData(itx).getDataElements(itx.x, itx.y, itx.width, itx.height, null);
-
-                        // blend overlapping regions using Porter-Duff alpha compositing: ar = a1 * (1 - a2) + a2
-                        for (int i = 0; i < resultData.length; i++) {
-                            int current = currentData[i] & 0xFF;
-                            if (current != 0) {
-                                int cumulative = resultData[i] & 0xFF;
-                                if (cumulative != 0) {
-                                    resultData[i] = (byte) ((cumulative * (0xff - current)) / 0x100 + current);
-                                } else
-                                    resultData[i] = (byte) current;
-                            }
-                        }
+                        maskImage = affinedImage;
                     }
-
-                    result.setDataElements(itx.x, itx.y, itx.width, itx.height, resultData);
                 }
             }
+
+            if (!maskImage.getBounds().intersects(rect))
+                continue;
+
+            Rectangle itx = maskImage.getBounds().intersection(rect);
+
+            byte resultData[];
+            if (!overlay) {
+                resultData = (byte[]) maskImage.getData(itx).getDataElements(itx.x, itx.y, itx.width, itx.height, null);
+                overlay = true;
+            } else {
+                resultData = (byte[]) result.getDataElements(itx.x, itx.y, itx.width, itx.height, null);
+                byte currentData[] = (byte[]) maskImage.getData(itx).getDataElements(itx.x, itx.y, itx.width, itx.height, null);
+
+                // blend overlapping regions using Porter-Duff alpha compositing: ar = a1 * (1 - a2) + a2
+                for (int i = 0; i < resultData.length; i++) {
+                    int current = currentData[i] & 0xFF;
+                    if (current != 0) {
+                        int cumulative = resultData[i] & 0xFF;
+                        if (cumulative != 0) {
+                            resultData[i] = (byte) ((cumulative * (0xff - current)) / 0x100 + current);
+                        } else
+                            resultData[i] = (byte) current;
+                    }
+                }
+            }
+
+            result.setDataElements(itx.x, itx.y, itx.width, itx.height, resultData);
         }
 
         return result;
     }
 
+    @Override
     public Raster getTile(int tileX, int tileY) {
         return getData(new Rectangle(tileXToX(tileX), tileYToY(tileY), getTileWidth(), getTileHeight()));
     }

commit 83be802e97df86f6ddb418d8e7cfbe292a473d48

Fix wrong region scale and position
This bug introduced by merge mistake in 704f706

lightcrafts/src/com/lightcrafts/jai/opimage/ShapedMask.java
@@ -316,34 +316,7 @@ public class ShapedMask extends PlanarImage {
                 if (scaledImage.scale < 1) {
                     float scaleX = (float) Math.floor(maskImage.getWidth() / scaledImage.scale) / (float) maskImage.getWidth();
                     float scaleY = (float) Math.floor(maskImage.getHeight() / scaledImage.scale) / (float) maskImage.getHeight();
-
-                    if (scaledImage.tx != 0 || scaledImage.ty != 0)
-                        transform.concatenate(AffineTransform.getTranslateInstance(scaledImage.tx, scaledImage.ty));
-
-                    Rectangle scaledBounds = transform.createTransformedShape(maskImage.getBounds()).getBounds();
-
-                    // Avoid scaling underflows resulting into exeptions
-                    if (scaledBounds.width < 3 || scaledBounds.height < 3)
-                        continue;
-
-                    synchronized (expandedMasks) {
-                        AffinedImage key = new AffinedImage(maskImage, transform);
-                        PlanarImage affinedImage = expandedMasks.get(key);
-                        if (affinedImage == null) {
-                            RenderingHints hints = new RenderingHints(JAI.KEY_BORDER_EXTENDER,
-                                                                      BorderExtender.createInstance(BorderExtender.BORDER_COPY));
-                            // hints.add(JAIContext.noCacheHint);
-                            Interpolation interp = Interpolation.getInstance(Interpolation.INTERP_BILINEAR);
-                            ParameterBlock params = new ParameterBlock();
-                            params.addSource(maskImage);
-                            params.add(transform);
-                            params.add(interp);
-                            maskImage = JAI.create("Affine", params, hints);
-                            expandedMasks.put(key, maskImage);
-                        } else {
-                            maskImage = affinedImage;
-                        }
-                    }
+                    transform.concatenate(AffineTransform.getScaleInstance(scaleX, scaleY));
                 }
 
                 if (scaledImage.tx != 0 || scaledImage.ty != 0)