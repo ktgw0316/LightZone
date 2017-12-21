2d873a5fca8ff78dc3722efa253f674561a272d4
KITAGAWA Masahiro <arctica0316@gmail.com>
2017-12-21 23:53:55 +0900

    macOS - Update postprocess.sh

--- macosx/postprocess.sh
+++ macosx/postprocess.sh
@@ -12,27 +12,27 @@ PATTERN="/usr/local/"
 
 cd $1
 
-FILES=$(find . -name "*.jnilib" -o -name "dcraw_lz")
+# Copy required dylibs, and change their ids
+DEPS=$(otool -L dcraw_lz *.jnilib | egrep -v "/(usr/lib|System)" | grep -o "/.*dylib" | sort -u)
+for DEP in ${DEPS}; do
+  LIB=$(basename ${DEP})
+  cp -f ${DEP} ${LIB}
+  chmod 755 ${LIB}
+  install_name_tool -id ${LIB} ${LIB}
+done
 
-# Copy required dylibs, and change its id
-for lib in ${FILES}; do
-  otool -L ${lib} | grep ${PATTERN} | while read line; do
-    fullpath=`echo ${line} | sed -e "s% *\(${PATTERN}[^ ]*\.dylib\).*%\1%"`
-    name=`echo ${fullpath} | sed -e "s%.*/\(.*\.dylib\)%\1%"`
-    cp -f ${fullpath} ${name}
-    chmod 755 ${name}
-    install_name_tool -id ${name} ${name} || continue
+# Change the local library paths in each file
+FILES=$(find . -name "*.jnilib" -a ! -name "libquaqua*" -o -name "*.dylib" -o -name "dcraw_lz")
+for FILE in ${FILES}; do
+  otool -L ${FILE} | egrep -v "$(otool -D ${FILE})" | egrep -v "/(usr/lib|System)" | grep -o "/.*\.dylib" | while read; do
+    install_name_tool -change ${REPLY} @executable_path/$(basename ${REPLY}) ${FILE}
   done
 done
 
-# Rewrite the library paths
-for lib in ${FILES}; do
-  otool -L ${lib} | grep ${PATTERN} | while read line; do
-    fullpath=`echo ${line} | sed -e "s% *\(${PATTERN}[^ ]*\.dylib\).*%\1%"`
-    name=`echo ${fullpath} | sed -e "s%.*/\(.*\.dylib\)%\1%"`
-    install_name_tool -change ${fullpath} @rpath/${name} ${lib} || continue
-  done
-done
-for lib in ${FILES}; do
-  install_name_tool -add_rpath @loader_path ${lib} || continue
-done
+# # Hack for homebrew libtiff, which uses libjpeg instead of libjpeg-turbo
+# tiff=$(find . -name "libtiff*.dylib")
+# turbo=$(find . -name "libjpeg*.dylib" | sed -e "s%^\.\/%%")
+# jpeg=$(otool -L ${tiff} | grep -o "@executable_path/libjpeg.*\.dylib")
+# install_name_tool -change ${jpeg} @executable_path/${turbo} ${tiff}
+
+# vim:set noet sw=8 ts=8:
