bd78d1b048fec8ed270e5d07e3574fb00413d46b
KITAGAWA Masahiro <arctica0316@gmail.com>
2019-05-26 16:07:22 +0900

    Repactor MetadataDirectoryModel

--- lightcrafts/src/com/lightcrafts/ui/metadata/MetadataDirectoryModel.java
+++ lightcrafts/src/com/lightcrafts/ui/metadata/MetadataDirectoryModel.java
@@ -1,11 +1,15 @@
 /* Copyright (C) 2005-2011 Fabio Riccardi */
+/* Copyright (C) 2019-     Masahiro Kitagawa */
 
 package com.lightcrafts.ui.metadata;
 
 import com.lightcrafts.image.metadata.ImageMetadataDirectory;
 import com.lightcrafts.image.metadata.values.ImageMetaValue;
+import lombok.Getter;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Represent an ImageMetadataDirectory as a String name plus a List of
@@ -13,47 +17,41 @@ import java.util.*;
  */
 class MetadataDirectoryModel {
 
+    @Getter
     private String name;
+
     private ArrayList<KeyValuePair> pairs;
 
-    static Comparator<KeyValuePair> KeyValuePairComp =
-        new Comparator<KeyValuePair>() {
-            public int compare(KeyValuePair pair1, KeyValuePair pair2) {
+    private static Comparator<KeyValuePair> KeyValuePairComp =
+            (pair1, pair2) -> {
                 String s1 = pair1.getKey();
                 String s2 = pair2.getKey();
                 return s1.compareTo(s2);
-            }
-        };
+            };
 
     MetadataDirectoryModel(
         ImageMetadataDirectory directory, boolean filter, boolean sort
     ) {
         name = directory.getName();
-        pairs = new ArrayList<KeyValuePair>();
-        Iterator<Map.Entry<Integer,ImageMetaValue>> i=directory.iterator();
-        while (i.hasNext()) {
-            Map.Entry<Integer,ImageMetaValue> entry = i.next();
+        pairs = new ArrayList<>();
+        directory.forEach(entry -> {
             Integer id = entry.getKey();
             ImageMetaValue value = directory.getValue(id);
-            if ( value.isDisplayable() ) {
+            if (value != null && value.isDisplayable()) {
                 String key = directory.getTagLabelFor(id);
                 KeyValuePair pair = new KeyValuePair(id, key, value);
                 pairs.add(pair);
             }
-        }
+        });
         if (filter) {
             pairs = MetaTagFilter.filter(directory, pairs);
         }
         if (sort) {
-            Collections.sort(pairs, KeyValuePairComp);
+            pairs.sort(KeyValuePairComp);
         }
     }
 
     List<KeyValuePair> getPairs() {
-        return new ArrayList<KeyValuePair>(pairs);
-    }
-
-    String getName() {
-        return name;
+        return new ArrayList<>(pairs);
     }
 }
