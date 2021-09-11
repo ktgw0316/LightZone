5b4987d9d37fb2b857b1a4d491fcd5ae41fd84d8
Masahiro Kitagawa <arctica0316@gmail.com>
2021-09-11 14:29:43 +0900

    Refactor deduplicate urgency value to color map

--- lightcrafts/src/com/lightcrafts/ui/metadata2/MetadataTable.java
+++ lightcrafts/src/com/lightcrafts/ui/metadata2/MetadataTable.java
@@ -15,8 +15,6 @@ import javax.swing.table.TableCellRenderer;
 import java.awt.*;
 import java.awt.event.*;
 import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
 
 class MetadataTable extends JTable {
 
@@ -126,24 +124,11 @@ class MetadataTable extends JTable {
             @Override
             public Component getListCellRendererComponent(JList<? extends String> list,
                     String value, int index, boolean isSelected, boolean cellHasFocus) {
-                val color = valueToColorMap.get(index);
+                val color = UrgencyObject.colorOf(index);
                 setBackground(color);
                 setText(Integer.valueOf(index).toString()); // DEBUG
                 return this;
             }
-
-            private final Map<Integer, Color> valueToColorMap = new HashMap<>() {{
-                // cf. https://jfly.uni-koeln.de/colorset/CUD_color_set_GuideBook_2018_for_print_cs4.pdf
-                put(0, new Color(0, 0, 0, 0)); // transparent
-                put(1, new Color(255, 75, 0)); // red
-                put(2, new Color(246, 170, 0)); // orange
-                put(3, new Color(255, 241, 0)); // yellow
-                put(4, new Color(3, 175, 122)); // green
-                put(5, new Color(77, 196, 255)); // blue
-                put(6, new Color(153, 0, 153)); // purple
-                put(7, new Color(132, 145, 158)); // gray
-                put(8, Color.BLACK);
-            }};
         }
 
         val urgencyCombo = new JComboBox<String>();
--- lightcrafts/src/com/lightcrafts/ui/metadata2/UrgencyMetadataEntry.java
+++ lightcrafts/src/com/lightcrafts/ui/metadata2/UrgencyMetadataEntry.java
@@ -13,6 +13,10 @@ import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.jetbrains.annotations.NotNull;
 
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.Map;
+
 public class UrgencyMetadataEntry extends SimpleMetadataEntry {
 
     @RequiredArgsConstructor
@@ -24,6 +28,27 @@ public class UrgencyMetadataEntry extends SimpleMetadataEntry {
         public String toString() {
             return value.toString();
         }
+
+        public Color getColor() {
+            return colorOf(value);
+        }
+
+        static public Color colorOf(int i) {
+            return valueToColorMap.get(i);
+        }
+
+        static private final Map<Integer, Color> valueToColorMap = new HashMap<>() {{
+            // cf. https://jfly.uni-koeln.de/colorset/CUD_color_set_GuideBook_2018_for_print_cs4.pdf
+            put(0, new Color(0, 0, 0, 0)); // transparent
+            put(1, new Color(255, 75, 0)); // red
+            put(2, new Color(246, 170, 0)); // orange
+            put(3, new Color(255, 241, 0)); // yellow
+            put(4, new Color(3, 175, 122)); // green
+            put(5, new Color(77, 196, 255)); // blue
+            put(6, new Color(153, 0, 153)); // purple
+            put(7, new Color(132, 145, 158)); // gray
+            put(8, Color.BLACK);
+        }};
     }
 
     UrgencyMetadataEntry() {
