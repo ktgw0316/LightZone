aa1426cd810d1606668dcdadf7a315f6143b1497
Masahiro Kitagawa <arctica0316@gmail.com>
2022-01-23 22:02:28 +0900

    Remove unused .png icons

--- lightcrafts/src/com/lightcrafts/app/PrintDocButton.java
+++ lightcrafts/src/com/lightcrafts/app/PrintDocButton.java
@@ -2,18 +2,18 @@
 
 package com.lightcrafts.app;
 
-import static com.lightcrafts.app.Locale.LOCALE;
-import com.lightcrafts.ui.toolkit.IconFactory;
 import com.lightcrafts.ui.editor.EditorMode;
+import com.lightcrafts.ui.toolkit.IconFontFactory;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import static com.lightcrafts.app.Locale.LOCALE;
+
 final class PrintDocButton extends EditorButton {
 
-    private final static Icon Icon =
-        IconFactory.createInvertedIcon(PrintDocButton.class, "print.png");
+    private final static Icon Icon = IconFontFactory.buildIcon("print");
 
     private final static String ToolTip = LOCALE.get("PrintDocButtonToolTip");
 
