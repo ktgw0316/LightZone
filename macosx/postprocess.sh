ed91af51441134d134ae619f35114db8c3b7ac38
Masahiro Kitagawa <arctica0316@gmail.com>
2022-06-18 22:53:26 +0900

    fix(mac): Crash with Namespace CODESIGNING, Code 2
    - This crash occured only on m1 mac

--- macosx/postprocess.sh
+++ macosx/postprocess.sh
@@ -17,11 +17,16 @@ FILES=$(find . -maxdepth 1 -name "*.jnilib" -o -name "dcraw_lz")
 sh ${SCRIPT_DIR}/copydylibs.sh ${FILES}
 
 # Change the local library paths in each file
-LIBS="${FILES} $(find . -maxdepth 1 -name "*.dylib")"
-for LIB in ${LIBS}; do
+DYLIBS=$(find . -maxdepth 1 -name "*.dylib")
+for LIB in ${FILES} ${DYLIBS}; do
   otool -LX ${LIB} | egrep -v "$(otool -DX ${LIB})" | egrep -v "/(usr/lib|System)" | grep -o "/.*\.dylib" | while read; do
     sh -x -c "install_name_tool -change ${REPLY} @loader_path/$(basename ${REPLY}) ${LIB}"
   done
+done
+
+# Sign the dylibs
+for DYLIB in ${DYLIBS}; do
+  codesign --force --deep --sign - ${DYLIB}
 done
 
 # vim:set noet sw=8 ts=8:
