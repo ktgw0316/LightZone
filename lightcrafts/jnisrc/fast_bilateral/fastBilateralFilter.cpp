f9d84aa2aa28cef04c0db22ca8b001a27fd0394c
Masahiro Kitagawa <arctica0316@gmail.com>
2024-03-06 12:19:12 +0900

    refactor(jni): Remove unused native funcs in fast_bilateral
    + Lint
    + Enable OpenMP

--- lightcrafts/jnisrc/fast_bilateral/fastBilateralFilter.cpp
+++ lightcrafts/jnisrc/fast_bilateral/fastBilateralFilter.cpp
@@ -1,26 +1,21 @@
 /* Copyright (C) 2005-2011 Fabio Riccardi */
 
-// standard
 #include <cmath>
-
-#include <algorithm>
-#include <fstream>
-#include <iostream>
-#include <sstream>
+#include <cstdint>
 
 #define NO_XML
-// #define CHRONO
 #include "include/fast_lbf.h"
-#include "include/geom.h"
 #include "mathlz.h"
 
-using namespace std;
-
 #include <jni.h>
+#include <omp.h>
+
+using std::ceil;
+using std::clamp;
 
 typedef Array_2D<float> image_type;
 
-extern "C" JNIEXPORT void JNICALL Java_com_lightcrafts_jai_opimage_FastBilateralFilterOpImage_fastBilateralFilterMono
+JNIEXPORT void JNICALL Java_com_lightcrafts_jai_opimage_FastBilateralFilterOpImage_fastBilateralFilterMono
 (   JNIEnv *env, jclass cls,
     jshortArray jsrcData, jshortArray jdestData,
     jfloat sigma_s, jfloat sigma_r,
@@ -30,50 +25,53 @@ extern "C" JNIEXPORT void JNICALL Java_com_lightcrafts_jai_opimage_FastBilateral
     jint srcLineStride, jint destLineStride,
     jfloatArray jtransform)
 {
-    unsigned short *srcData = (unsigned short *) env->GetPrimitiveArrayCritical(jsrcData, 0);
-    unsigned short *destData = (unsigned short *) env->GetPrimitiveArrayCritical(jdestData, 0);
-    float *transform = (float *) env->GetPrimitiveArrayCritical(jtransform, 0);
+    const auto srcData = static_cast<uint16_t*>(env->GetPrimitiveArrayCritical(jsrcData, 0));
+    const auto destData = static_cast<uint16_t*>(env->GetPrimitiveArrayCritical(jdestData, 0));
+    const auto transform = static_cast<float*>(env->GetPrimitiveArrayCritical(jtransform, 0));
+
+    const float inv_max = 1.0f / 0xffff;
+
+    image_type image(width, height);
+    image_type profile(width, height);
+
+#pragma omp parallel for
+    for(int y = 0; y < height; y++) {
+#pragma omp simd
+        for(int x = 0; x < width; x++) {
+            int g = srcData[srcPixelStride * x + y * srcLineStride + srcOffset];
+            image(x, y) = g * inv_max;
+            profile(x, y) = transform[g];
+        }
+    }
 
     const int padding = 2 * ceil(sigma_s);
 
-    image_type image(width,height);
-    image_type profile(width,height);
+    image_type filtered_image(width-2*padding, height-2*padding);
+    image_type weight(width-2*padding, height-2*padding);
 
-    for(unsigned y = 0; y < height; y++)
-        for(unsigned x = 0; x < width; x++) {
-            int g = srcData[srcPixelStride * x + y * srcLineStride + srcOffset];
-            image(x,y) = g / (float) 0xffff;
-            profile(x,y) = transform[g];
-        }
+    Image_filter::fast_LBF(image, profile, sigma_s, sigma_r, &weight, &filtered_image);
 
-    image_type filtered_image(width-2*padding,height-2*padding);
-    image_type weight(width-2*padding,height-2*padding);
+    const float inv_sq_sigma_s = 1.0f / (sigma_s * sigma_s);
 
-    Image_filter::fast_LBF(image, profile,
-                           sigma_s, sigma_r,
-                           false,
-                           &weight, &filtered_image);
-
-    for(unsigned y = 0; y < height - 2 * padding; y++)
-        for(unsigned x = 0; x < width - 2 * padding; x++) {
+#pragma omp parallel for
+    for(int y = 0; y < height - 2 * padding; y++) {
+#pragma omp simd
+        for(int x = 0; x < width - 2 * padding; x++) {
             const float BF = filtered_image(x, y);
-            const float W = weight(x, y)/(sigma_s*sigma_s);
-            const unsigned short bf = static_cast<unsigned short>(Math_tools::clamp(0.0, 0xffff, BF * 0xffff));
+            const float W = weight(x, y) * inv_sq_sigma_s;
+            const auto bf = static_cast<uint16_t>(clamp(BF * 0xffff, 0.0f, static_cast<float>(0xffff)));
+            const auto w =  static_cast<uint16_t>(clamp(W * 0xffff, 0.0f, static_cast<float>(0xffff)));
             destData[destPixelStride * x + y * destLineStride + destOffset] = bf;
-            const unsigned short w = static_cast<unsigned short>(Math_tools::clamp(0.0, 0xffff, W * 0xffff));
             destData[destPixelStride * x + y * destLineStride + destOffset + 1] = w;
         }
+    }
 
     env->ReleasePrimitiveArrayCritical(jsrcData, srcData, 0);
     env->ReleasePrimitiveArrayCritical(jdestData, destData, 0);
     env->ReleasePrimitiveArrayCritical(jtransform, transform, 0);
 }
 
-inline float fsqrt(float x) {
-    return x == 0 ? 0 : x * inv_sqrt(x);
-}
-
-extern "C" JNIEXPORT void JNICALL Java_com_lightcrafts_jai_opimage_FastBilateralFilterOpImage_fastBilateralFilterChroma
+JNIEXPORT void JNICALL Java_com_lightcrafts_jai_opimage_FastBilateralFilterOpImage_fastBilateralFilterChroma
 (   JNIEnv *env, jclass cls,
     jshortArray jsrcData, jshortArray jdestData,
     jfloat sigma_s, jfloat sigma_r,
@@ -83,54 +81,47 @@ extern "C" JNIEXPORT void JNICALL Java_com_lightcrafts_jai_opimage_FastBilateral
     jint destROffset, jint destGOffset, jint destBOffset,
     jint srcLineStride, jint destLineStride )
 {
-    unsigned short *srcData = (unsigned short *) env->GetPrimitiveArrayCritical(jsrcData, 0);
-    unsigned short *destData = (unsigned short *) env->GetPrimitiveArrayCritical(jdestData, 0);
+    const auto srcData = static_cast<uint16_t*>(env->GetPrimitiveArrayCritical(jsrcData, 0));
+    const auto destData = static_cast<uint16_t*>(env->GetPrimitiveArrayCritical(jdestData, 0));
 
-    // image_type l_image(width,height);
-    image_type a_image(width,height);
-    image_type b_image(width,height);
+    const float inv_max = 1.0f / 0xffff;
 
-    for(unsigned y = 0; y < height; y++)
-        for(unsigned x = 0; x < width; x++) {
-            float l = srcData[srcPixelStride * x + y * srcLineStride + srcROffset] / (float) 0xffff;
-            float a = srcData[srcPixelStride * x + y * srcLineStride + srcGOffset] / (float) 0xffff;
-            float b = srcData[srcPixelStride * x + y * srcLineStride + srcBOffset] / (float) 0xffff;
+    image_type a_image(width, height);
+    image_type b_image(width, height);
 
-            // l_image(x,y) = fsqrt(l);
-            a_image(x,y) = a;
-            b_image(x,y) = b;
+#pragma omp parallel for
+    for(int y = 0; y < height; y++) {
+#pragma omp simd
+        for(int x = 0; x < width; x++) {
+            float a = srcData[srcPixelStride * x + y * srcLineStride + srcGOffset] * inv_max;
+            float b = srcData[srcPixelStride * x + y * srcLineStride + srcBOffset] * inv_max;
+            a_image(x, y) = a;
+            b_image(x, y) = b;
         }
+    }
 
-    image_type filtered_a_image(width,height);
-    image_type filtered_b_image(width,height);
+    image_type filtered_a_image(width, height);
+    image_type filtered_b_image(width, height);
 
-    Image_filter::fast_LBF(a_image, a_image,
-                           sigma_s, sigma_r,
-                           false,
-                           &filtered_a_image, &filtered_a_image);
+    Image_filter::fast_LBF(a_image, a_image, sigma_s, sigma_r, &filtered_a_image, &filtered_a_image);
+    Image_filter::fast_LBF(b_image, b_image, sigma_s, sigma_r, &filtered_b_image, &filtered_b_image);
 
-    Image_filter::fast_LBF(b_image, b_image,
-                           sigma_s, sigma_r,
-                           false,
-                           &filtered_b_image, &filtered_b_image);
+    const int padding = 2 * ceil(sigma_s);
 
-    int padding = 2 * ceil(sigma_s);
-
-    for(unsigned y = 0; y < height - 2 * padding; y++)
-        for(unsigned x = 0; x < width - 2 * padding; x++) {
+#pragma omp parallel for
+    for(int y = 0; y < height - 2 * padding; y++) {
+#pragma omp simd
+        for(int x = 0; x < width - 2 * padding; x++) {
             const float A = filtered_a_image(x+padding, y+padding);
             const float B = filtered_b_image(x+padding, y+padding);
-
-            int l = srcData[srcPixelStride * (x+padding) + (y+padding) * srcLineStride + srcROffset];
-
+            const auto l = srcData[srcPixelStride * (x+padding) + (y+padding) * srcLineStride + srcROffset];
+            const auto a = static_cast<uint16_t>(clamp(A * 0xffff, 0.0f, static_cast<float>(0xffff)));
+            const auto b = static_cast<uint16_t>(clamp(B * 0xffff, 0.0f, static_cast<float>(0xffff)));
             destData[destPixelStride * x + y * destLineStride + destROffset] = l;
-
-            const unsigned short a = static_cast<unsigned short>(Math_tools::clamp(0.0, 0xffff, A * 0xffff));
             destData[destPixelStride * x + y * destLineStride + destGOffset] = a;
-
-            const unsigned short b = static_cast<unsigned short>(Math_tools::clamp(0.0, 0xffff, B * 0xffff));
             destData[destPixelStride * x + y * destLineStride + destBOffset] = b;
         }
+    }
 
     env->ReleasePrimitiveArrayCritical(jsrcData, srcData, 0);
     env->ReleasePrimitiveArrayCritical(jdestData, destData, 0);
--- lightcrafts/jnisrc/fast_bilateral/include/fast_lbf.h
+++ lightcrafts/jnisrc/fast_bilateral/include/fast_lbf.h
@@ -3,48 +3,22 @@
 #ifndef __FAST_LINEAR_BF__
 #define __FAST_LINEAR_BF__
 
+#include <array>
 #include "array.h"
 #include "math_tools.h"
 #include "mixed_vector.h"
-
-#ifdef CHRONO
-#  include <iostream>
-#  include "chrono.h"
-#endif
+#include "omp.h"
 
 namespace Image_filter{
 
   template <typename Base_array,typename Data_array,typename Real>
   void fast_LBF(const Data_array&  input,
-		const Base_array&  base,
-		const Real         space_sigma,
-		const Real         range_sigma,
-		const bool         early_division,
-		Base_array* const  weight,
-		Data_array* const  result);
+        const Base_array&  base,
+        const Real         space_sigma,
+        const Real         range_sigma,
+        Base_array* const  weight,
+        Data_array* const  result);
 
-  template <typename Data_array,typename Real>
-  void fast_LBF(const Data_array& input,
-		const Real        space_sigma,
-		const Real        range_sigma,
-		Data_array* const result);
-  
-  
-  template <typename Base_array,typename Data_array,typename Weight_array,typename Real>
-      void fast_color_BF(const Data_array&   input,
-                         const Base_array&   base,
-                         const Real          space_sigma,
-                         const Real          range_sigma,
-                         Weight_array* const weight,
-                         Data_array* const   result);
-  
-  template <typename Data_array,typename Real>
-      void fast_color_BF(const Data_array& input,
-                         const Real        space_sigma,
-                         const Real        range_sigma,
-                         Data_array* const result);
-  
-  
   /*
    
 #############################################
@@ -60,26 +34,13 @@ namespace Image_filter{
 */
 
 
-  template <typename Data_array,typename Real>
-  void fast_LBF(const Data_array& input,
-		const Real        space_sigma,
-		const Real        range_sigma,
-		Data_array* const result){
-
-    fast_LBF(input,input,
-	     space_sigma,range_sigma,
-	     true,result,result);
-  }
-
-
   template <typename Base_array,typename Data_array,typename Real>
   void fast_LBF(const Data_array&  input,
-		const Base_array&  base,
-		const Real         space_sigma,
-		const Real         range_sigma,
-		const bool         early_division,
-		Base_array* const  weight,
-		Data_array* const  result){
+        const Base_array&  base,
+        const Real         space_sigma,
+        const Real         range_sigma,
+        Base_array* const  weight,
+        Data_array* const  result){
 
     using namespace std;
 
@@ -89,74 +50,51 @@ namespace Image_filter{
     typedef Mixed_vector<data_type,base_type> mixed_type;
     typedef unsigned int                      size_type;
     
-    typedef Base_array           base_array_2D_type;
     typedef Array_3D<mixed_type> mixed_array_3D_type;
 
     const size_type width  = result->x_size();
     const size_type height = result->y_size();
 
-    const size_type padding_xy = 2;
-    const size_type padding_z  = 2;
+    constexpr size_type padding_xy = 2;
+    constexpr size_type padding_z  = 2;
     
-    int padding = 2 * ceil(space_sigma);
+    const int padding = 2 * ceil(space_sigma);
 
-    const base_type base_min   = 0; // *min_element(base.begin(),base.end());
-    const base_type base_max   = 1; // *max_element(base.begin(),base.end());
-    const base_type base_delta = 1; // base_max - base_min;
+    constexpr base_type base_min   = 0; // *min_element(base.begin(),base.end());
+    constexpr base_type base_max   = 1; // *max_element(base.begin(),base.end());
+    constexpr base_type base_delta = base_max - base_min;
     
-    // fprintf(stderr, "min: %f, max: %f, delta: %f\n", base_min, base_max, base_delta);
-    
-    const size_type small_width  = static_cast<size_type>((width  - 1) / space_sigma) + 2 + 2 * padding_xy; 
+    const size_type small_width  = static_cast<size_type>((width  - 1) / space_sigma) + 2 + 2 * padding_xy;
     const size_type small_height = static_cast<size_type>((height - 1) / space_sigma) + 2 + 2 * padding_xy; 
     const size_type small_depth  = static_cast<size_type>(base_delta / range_sigma)   + 1 + 2 * padding_z; 
 
-    
-#ifdef CHRONO
-    Chrono chrono("filter");
-    chrono.start();
-
-//     for(size_type N = 0;N < 1000;++N){
-
-    Chrono chrono_down("downsampling");
-    chrono_down.start();
-#endif
-    
     mixed_array_3D_type data(small_width,
-			     small_height,
-			     small_depth,mixed_type());
+                 small_height,
+                 small_depth,mixed_type());
 
-    for(size_type x = 0,x_end = width+2*padding;x < x_end;++x){
+#pragma omp parallel for
+    for(size_type x = 0;x < width+2*padding; ++x){
+      for(size_type y = 0; y < height+2*padding; ++y){
 
-      const size_type small_x = static_cast<size_type>(x / space_sigma + 0.5);
+        const base_type z = base(x,y) - base_min;
 
-      for(size_type y = 0,y_end = height+2*padding;y < y_end;++y){
-
-	const base_type z = base(x,y) - base_min;
-	
-	const size_type small_y = static_cast<size_type>(y / space_sigma + 0.5);
-	const size_type small_z = static_cast<size_type>(z / range_sigma + 0.5) + padding_z;
+        const size_type small_x = static_cast<size_type>(x / space_sigma + 0.5);
+        const size_type small_y = static_cast<size_type>(y / space_sigma + 0.5);
 
         if (small_x < small_width && small_y < small_height) {
+            const size_type small_z = static_cast<size_type>(z / range_sigma + 0.5) + padding_z;
             mixed_type& d = data(small_x,small_y,small_z);
-    // 	cout<<d.first<<" "<<input(x,y)<<endl;
             d.first  += input(x,y);
             d.second += 1.0;
         }
       } // END OF for y
     } // END OF for x
 
-#ifdef CHRONO
-    chrono_down.stop();
-    cout<<"  "<<chrono_down.report()<<endl;
-
-    Chrono chrono_convolution("convolution");
-    chrono_convolution.start();
-#endif  
-
-    vector<int> offset(3);
-    offset[0] = &(data(1,0,0)) - &(data(0,0,0));
-    offset[1] = &(data(0,1,0)) - &(data(0,0,0));
-    offset[2] = &(data(0,0,1)) - &(data(0,0,0));
+    const array<intptr_t, 3> offset {
+        &(data(1,0,0)) - &(data(0,0,0)),
+        &(data(0,1,0)) - &(data(0,0,0)),
+        &(data(0,0,1)) - &(data(0,0,0))
+    };
     
     mixed_array_3D_type buffer(small_width,small_height,small_depth);
 
@@ -166,301 +104,56 @@ namespace Image_filter{
       
       for(size_type n_iter = 0;n_iter < 2;++n_iter){
 
-	swap(buffer,data);
+        swap(buffer,data);
       
-	for(size_type
-	      x     = 1,
-	      x_end = small_width - 1;
-	    x < x_end;++x){
+        for(size_type
+            x     = 1,
+            x_end = small_width - 1;
+            x < x_end;++x){
 
-	  for(size_type
-		y     = 1,
-		y_end = small_height - 1;
-	      y < y_end;++y){
+          for(size_type
+              y     = 1,
+              y_end = small_height - 1;
+              y < y_end;++y){
 
-	    mixed_type* d_ptr = &(data(x,y,1));
-	    mixed_type* b_ptr = &(buffer(x,y,1));
-	  
-	    for(size_type
-		  z     = 1,
-		  z_end = small_depth - 1;
-		z < z_end;
-		++z,++d_ptr,++b_ptr){
-	      
-	      *d_ptr = (*(b_ptr - off) + *(b_ptr + off) + 2.0 * (*b_ptr)) / 4.0;
+            mixed_type* d_ptr = &(data(x,y,1));
+            mixed_type* b_ptr = &(buffer(x,y,1));
 
-	    } // END OF for z
-	  } // END OF for y
-	} // END OF for x
+            for(size_type
+                z     = 1,
+                z_end = small_depth - 1;
+                z < z_end;
+                ++z,++d_ptr,++b_ptr){
+
+              *d_ptr = (*(b_ptr - off) + *(b_ptr + off) + 2.0 * (*b_ptr)) / 4.0;
+
+            } // END OF for z
+          } // END OF for y
+        } // END OF for x
       } // END OF for n_iter
     } // END OF for dim
 
-    
-#ifdef CHRONO
-    chrono_convolution.stop();
-    cout<<"  "<<chrono_convolution.report()<<endl;
-
-    Chrono chrono_nonlinearities("nonlinearities");
-    chrono_nonlinearities.start();
-#endif
-
     result->resize(width,height);
     
-    if (early_division){
+    weight->resize(width,height);
 
-      for(typename mixed_array_3D_type::iterator
-	    d     = data.begin(),
-	    d_end = data.end();
-	  d != d_end;++d){
-	*d /= (d->second != 0) ? d->second : 1;
-      }
-      
-      for(size_type x=0,x_end=width;x<x_end;x++){
-	for(size_type y=0,y_end=height;y<y_end;y++){
-      
-	  const base_type z = base(x + padding,y + padding) - base_min;
+#pragma omp parallel for
+    for(size_type y = 0; y < height; ++y){
+      for(size_type x = 0; x < width; ++x){
 
-	  const mixed_type D =
-	    Math_tools::trilinear_interpolation(data,
-						static_cast<real_type>(x) / space_sigma + padding_xy,
-						static_cast<real_type>(y) / space_sigma + padding_xy,
-						z / range_sigma + padding_z);
-	  
-	  (*result)(x,y) = D.first;
-	  
-	} // END OF for y
+        const base_type z = base(x + padding,y + padding) - base_min;
+
+        const mixed_type D =
+          Math_tools::trilinear_interpolation(data,
+                          static_cast<real_type>(x) / space_sigma + padding_xy,
+                          static_cast<real_type>(y) / space_sigma + padding_xy,
+                          z / range_sigma + padding_z);
+
+        (*weight)(x,y) = D.second;
+        (*result)(x,y) = D.first / D.second;
       } // END OF for x
-      
-    }
-    else{ // END OF if early_division
-
-      weight->resize(width,height);
-      
-      for(size_type x = 0,x_end = width;x < x_end;++x){
-	for(size_type y = 0,y_end = height;y < y_end;++y){
-      
-	  const base_type z = base(x + padding,y + padding) - base_min;
-
-	  const mixed_type D =
-	    Math_tools::trilinear_interpolation(data,
-						static_cast<real_type>(x) / space_sigma + padding_xy,
-						static_cast<real_type>(y) / space_sigma + padding_xy,
-						z / range_sigma + padding_z);      
-
-	  (*weight)(x,y) = D.second;
-	  (*result)(x,y) = D.first / D.second;
-
-// 	  cout <<x<<" "<<y<<" "<<z<<" "
-// 	       <<D.first<<" "<<D.second<<" "
-// 	       << (*result)(x,y) << endl;
-	} // END OF for y
-      } // END OF for x
-    } // END OF else early_division
-
-#ifdef CHRONO
-    chrono_nonlinearities.stop();
-    cout<<"  "<<chrono_nonlinearities.report()<<endl;
-//     }    
-    chrono.stop();    
-    cout<<"  "<<chrono.report()<<endl;
-#endif
-
+    } // END OF for y
   }
-
-  /// *** COLOR ***
-  
-  template <typename Data_array,typename Real>
-      void fast_color_BF(const Data_array& input,
-                         const Real        space_sigma,
-                         const Real        range_sigma,
-                         Data_array* const result){
-          
-          Array_2D<Real> weight;
-          fast_color_BF(input,input,
-                        space_sigma,range_sigma,
-                        &weight,result);
-      }
-  
-  
-  template <typename Base_array,typename Data_array,typename Weight_array,typename Real>
-      void fast_color_BF(const Data_array&   input,
-                         const Base_array&   base,
-                         const Real          space_sigma,
-                         const Real          range_sigma,
-                         Weight_array* const weight,
-                         Data_array* const   result){
-          
-          using namespace std;
-          typedef typename Base_array::value_type   base_type;
-          typedef float                             real_type;
-          typedef typename Data_array::value_type   data_type;
-          typedef Mixed_vector<data_type,real_type> mixed_type;
-          typedef unsigned int                      size_type;
-          
-          const size_type dimension = 5;
-          
-          typedef Base_array                             base_array_2D_type;
-          typedef Array_ND<dimension,mixed_type>         mixed_array_ND_type;
-          typedef typename mixed_array_ND_type::key_type key_type;
-          
-          const size_type width  = input.x_size();
-          const size_type height = input.y_size();
-          
-          const size_type padding_s = 2;
-          const size_type padding_r = 2;
-          
-          key_type padding;
-          padding[0] = padding_s;
-          padding[1] = padding_s;
-          padding[2] = padding_r;
-          padding[3] = padding_r;
-          padding[4] = padding_r;
-          
-          base_type base_min;
-          key_type size,index = key_type();
-          
-          for(size_type c = 0;c < 3;++c){
-              
-              real_type m = base[index][c];
-              real_type M = m;
-              
-              for(typename base_array_2D_type::const_iterator
-                  b     = base.begin(),
-                  b_end = base.end();
-                  b != b_end;++b){
-                  
-                  const real_type r = (*b)[c];
-                  m = min(m,r);
-                  M = max(M,r);
-              }
-              
-              base_min[c] = m;
-              size[2 + c] = static_cast<size_type>((M - m) / range_sigma)
-                  + 1 + 2 * padding[2 + c];
-          }
-          size[0] = static_cast<size_type>((width  - 1) / space_sigma) + 1 + 2*padding[0]; 
-          size[1] = static_cast<size_type>((height - 1) / space_sigma) + 1 + 2*padding[1]; 
-          
-#ifdef CHRONO
-          Chrono chrono("filter");
-          chrono.start();
-          
-          Chrono chrono_down("downsampling");
-          chrono_down.start();
-#endif
-          
-          
-          mixed_array_ND_type data(size,mixed_type());
-          
-          for(size_type x = 0,x_end = width;x < x_end;++x){
-              
-              index[0] = static_cast<size_type>(1.0*x / space_sigma + 0.5) + padding[0];
-              
-              for(size_type y = 0,y_end = height;y < y_end;++y){
-                  
-                  index[1] = static_cast<size_type>(1.0*y / space_sigma + 0.5) + padding[1];
-                  
-                  for(size_type c = 0;c < 3;++c){
-                      index[2 + c] =
-                      static_cast<size_type>((base(x,y)[c] - base_min[c]) / range_sigma + 0.5)
-                      + padding[2 + c];
-                  }
-                  
-                  mixed_type& d = data[index];
-                  d.first  += input(x,y);
-                  d.second += 1.0;
-              } // END OF for y
-          } // END OF for x
-          
-#ifdef CHRONO
-          chrono_down.stop();
-          cout<<"  "<<chrono_down.report()<<endl;
-          
-          Chrono chrono_convolution("convolution");
-          chrono_convolution.start();
-#endif  
-          
-          mixed_array_ND_type buffer(size);
-          
-          const mixed_type* const origin = &(data[key_type()]);
-          key_type start_index,end_index;
-          
-          for(size_type dim = 0;dim < dimension;++dim){
-              start_index[dim] = 1;
-              end_index[dim]   = (dim == dimension - 1) ? 2 : (size[dim] - 1);
-          }
-          
-          for(size_type dim = 0;dim < dimension;++dim){
-              
-              key_type offset_proxy;
-              offset_proxy[dim] = 1;
-              const int off = &(data[offset_proxy]) - origin;  
-              
-              for(size_type n_iter = 0;n_iter < 2;++n_iter){
-                  
-                  swap(buffer,data);
-                  
-                  key_type index = start_index;
-                  do{
-                      mixed_type* d_ptr = &(data[index]);
-                      mixed_type* b_ptr = &(buffer[index]);
-                      
-                      for(size_type
-                          z     = 1,
-                          z_end = size[dimension-1] - 1;
-                          z < z_end;
-                          ++z,++d_ptr,++b_ptr){
-                          
-                          *d_ptr = (*(b_ptr - off) + *(b_ptr + off) + 2.0 * (*b_ptr)) / 4.0;
-                          
-                      } // END OF for z
-                  }
-                  while(data.advance(&index,start_index,end_index));      
-              } // END OF for n_iter
-          } // END OF for dim
-          
-#ifdef CHRONO
-          chrono_convolution.stop();
-          cout<<"  "<<chrono_convolution.report()<<endl;
-          
-          Chrono chrono_nonlinearities("nonlinearities");
-          chrono_nonlinearities.start();
-#endif
-          
-          result->resize(width,height);
-          
-          vector<real_type> pos(dimension);
-          
-          
-          weight->resize(width,height);
-          
-          for(size_type x = 0,x_end = width;x < x_end;++x){
-              
-              pos[0] = static_cast<real_type>(x) / space_sigma + padding[0];
-              
-              for(size_type y = 0,y_end = height;y < y_end;++y){
-                  
-                  pos[1] = static_cast<real_type>(y) / space_sigma + padding[1];
-                  
-                  for(size_type c = 0;c < 3;++c){
-                      pos[2 + c] = (base(x,y)[c] - base_min[c]) / range_sigma + padding[2 + c];
-                  }
-                  const mixed_type D = Math_tools::Nlinear_interpolation(data,pos);	  
-                  
-                  (*weight)(x,y) = D.second;
-                  (*result)(x,y) = D.first / D.second;
-              } // END OF for y
-          } // END OF for x
-          
-#ifdef CHRONO
-          chrono_nonlinearities.stop();
-          cout<<"  "<<chrono_nonlinearities.report()<<endl;
-          
-          chrono.stop();    
-          cout<<"  "<<chrono.report()<<endl;
-#endif
-          
-      }
 
 } // END OF namespace Image_filter
 
