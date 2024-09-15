1fc8c2341399bd13a20cdd9c64036c1ba0298673
Masahiro Kitagawa <arctica0316@gmail.com>
2024-09-15 11:42:51 +0900

    fix(mac): "Library not loaded" error
    + Fix wrong id for dylibs
    + Fix libomp path for dcraw_lz

--- macosx/products/postprocess.sh
+++ macosx/products/postprocess.sh
@@ -12,7 +12,7 @@ SCRIPT_DIR=$(cd $(dirname $0) && pwd)
 
 cd $1
 
-# Copy required dylibs, and change their ids
+# Copy required dylibs
 FILES=$(find . -maxdepth 1 -name "*.jnilib" -o -name "dcraw_lz")
 sh ${SCRIPT_DIR}/copydylibs.sh ${FILES}
 
@@ -20,13 +20,21 @@ sh ${SCRIPT_DIR}/copydylibs.sh ${FILES}
 DYLIBS=$(find . -maxdepth 1 -name "*.dylib")
 for LIB in ${FILES} ${DYLIBS}; do
   otool -LX ${LIB} | egrep -v "$(otool -DX ${LIB})" | egrep -v "/(usr/lib|System)" | grep -o "/.*\.dylib" | while read; do
-    sh -x -c "install_name_tool -change ${REPLY} @loader_path/$(basename ${REPLY}) ${LIB}"
+    sh -x -c "install_name_tool -change ${REPLY} @rpath/$(basename ${REPLY}) ${LIB}"
   done
 done
 
-# Sign the dylibs
-for DYLIB in ${DYLIBS}; do
-  codesign --force --deep --sign - ${DYLIB}
+otool -LX dcraw_lz | egrep -v "/(usr/lib|System)" | grep -o "/.*\.dylib" | while read; do
+  sh -x -c "install_name_tool -change ${REPLY} @rpath/$(basename ${REPLY}) dcraw_lz"
+done
+
+for LIB in ${DYLIBS}; do
+    install_name_tool -id @rpath/$(basename ${LIB}) ${LIB}
+done
+
+# Sign the dylibs/executables
+for FILE in ${FILES} ${DYLIBS}; do
+  codesign --force --deep --sign - ${FILE}
 done
 
 # vim:set noet sw=8 ts=8:
