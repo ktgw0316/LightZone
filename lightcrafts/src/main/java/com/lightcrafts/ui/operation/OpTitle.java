8cdbbd4f82c2148ce4641752f956d13ef78427cb
Masahiro Kitagawa <arctica0316@gmail.com>
2024-09-25 08:08:32 +0900

    fix: Use CheckBox insted of PNGs to enable/disable Tools

--- lightcrafts/src/main/java/com/lightcrafts/ui/operation/OpTitle.java
+++ lightcrafts/src/main/java/com/lightcrafts/ui/operation/OpTitle.java
@@ -1,16 +1,18 @@
 /* Copyright (C) 2005-2011 Fabio Riccardi */
+/* Copyright (C) 2024-     Masahiro Kitagawa */
 
 package com.lightcrafts.ui.operation;
 
+import com.formdev.flatlaf.icons.FlatInternalFrameCloseIcon;
 import com.lightcrafts.model.Operation;
 import static com.lightcrafts.ui.operation.Locale.LOCALE;
-import com.lightcrafts.ui.toolkit.ImageOnlyButton;
 import com.lightcrafts.utils.xml.XMLException;
 import com.lightcrafts.utils.xml.XmlDocument;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.Dimension;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -28,8 +30,10 @@ class OpTitle extends SelectableTitle {
         Preferences.userNodeForPackage(OpTitle.class);
     private final static String PresetsKey = "Presets";
 
+    private final static Dimension removeButtonSize = new Dimension(16, 16);
+
     private OpControl control;
-    private JToggleButton activeButton;
+    private JCheckBox activeCheckBox;
     private JButton removeButton;
     private OpTitleEditor editor;
 
@@ -44,36 +48,27 @@ class OpTitle extends SelectableTitle {
         BufferedImage image = OpActions.getIcon(op);
         setIcon(image);
 
-        activeButton = createActiveButton();
-        activeButton.setToolTipText(LOCALE.get("DisableToolTip"));
-        activeButton.setSelected(true);
-        activeButton.addActionListener(
-            new ActionListener() {
-                public void actionPerformed(ActionEvent event) {
-                    boolean active = activeButton.isSelected();
-                    String tip;
-                    if (active) {
-                        tip = LOCALE.get("DisableToolTip");
-                    }
-                    else {
-                        tip = LOCALE.get("EnableToolTip");
-                    }
-                    activeButton.setToolTipText(tip);
-                    control.setActivated(activeButton.isSelected());
-                }
-            }
-        );
-        removeButton = createGoAwayButton();
+        activeCheckBox = new JCheckBox("", true);
+        activeCheckBox.setToolTipText(LOCALE.get("DisableToolTip"));
+        activeCheckBox.setRolloverEnabled(true);
+        activeCheckBox.setSelected(true);
+        activeCheckBox.addActionListener(event -> {
+            boolean active = activeCheckBox.isSelected();
+            String tip = active ? LOCALE.get("DisableToolTip") : LOCALE.get("EnableToolTip");
+            activeCheckBox.setToolTipText(tip);
+            control.setActivated(activeCheckBox.isSelected());
+        });
+
+        removeButton = new JButton(new FlatInternalFrameCloseIcon());
         removeButton.setToolTipText(LOCALE.get("RemoveToolTip"));
-        removeButton.addActionListener(
-            new ActionListener() {
-                public void actionPerformed(ActionEvent event) {
-                    stack.removeControl(control);
-                }
-            }
-        );
+        removeButton.setPreferredSize(removeButtonSize);
+        removeButton.setMaximumSize(removeButtonSize);
+        removeButton.setContentAreaFilled(false);
+        removeButton.setBorder(null);
+        removeButton.addActionListener(event -> stack.removeControl(control));
+
         if (! control.isRawCorrection()) {
-            buttonBox.add(activeButton);
+            buttonBox.add(activeCheckBox);
             // buttonBox.add(Box.createHorizontalStrut(ButtonSpace));
             buttonBox.add(removeButton);
             // buttonBox.add(Box.createHorizontalStrut(ButtonSpace));
@@ -87,7 +82,7 @@ class OpTitle extends SelectableTitle {
     void setActive(boolean active) {
         // Won't generate an ActionEvent, so no risk of recursion with the
         // activeButton ActionListener:
-        activeButton.setSelected(active);
+        activeCheckBox.setSelected(active);
     }
 
     void resetTitle(String title) {
@@ -96,36 +91,6 @@ class OpTitle extends SelectableTitle {
             // (Called from base class constructor, when editor may be null)
             editor.setLabel(label);
         }
-    }
-
-    private static JButton createGoAwayButton() {
-        Icon normalIcon = getTitleIcon("x");
-        Icon highlightIcon = getTitleIcon("x_H");
-        Icon pressedIcon = getTitleIcon("x_S");
-        JButton button = new ImageOnlyButton(
-            normalIcon, highlightIcon, pressedIcon, null
-        );
-//        button.putClientProperty(SubstanceLookAndFeel.BUTTON_NO_MIN_SIZE_PROPERTY, Boolean.TRUE);
-//        button.putClientProperty(SubstanceLookAndFeel.BUTTON_PAINT_NEVER_PROPERTY, Boolean.TRUE);
-//        ImageOnlyButton.setStyle(button);
-        return button;
-    }
-
-    private static JToggleButton createActiveButton() {
-        JToggleButton button = new JToggleButton();
-        Icon normalIcon = getTitleIcon("nocheck");
-        Icon selectedIcon = getTitleIcon("check");
-        Icon normalHighlightIcon = getTitleIcon("nocheck_H");
-        Icon selectedHighlightIcon = getTitleIcon("check_H");
-        button.setIcon(normalIcon);
-        button.setSelectedIcon(selectedIcon);
-        button.setRolloverIcon(normalHighlightIcon);
-        button.setRolloverSelectedIcon(selectedHighlightIcon);
-        button.setRolloverEnabled(true);
-//        button.putClientProperty(SubstanceLookAndFeel.BUTTON_NO_MIN_SIZE_PROPERTY, Boolean.TRUE);
-//        button.putClientProperty(SubstanceLookAndFeel.BUTTON_PAINT_NEVER_PROPERTY, Boolean.TRUE);
-        ImageOnlyButton.setStyle(button);
-        return button;
     }
 
     JPopupMenu getPopupMenu() {
