63472f06c6ee0b83a7bfa37177bcff40912c2777
Masahiro Kitagawa <arctica0316@gmail.com>
2024-09-25 08:10:30 +0900

    refactor: Simplify OpTitle methods

--- lightcrafts/src/main/java/com/lightcrafts/ui/operation/OpTitle.java
+++ lightcrafts/src/main/java/com/lightcrafts/ui/operation/OpTitle.java
@@ -8,14 +8,21 @@ import com.lightcrafts.model.Operation;
 import static com.lightcrafts.ui.operation.Locale.LOCALE;
 import com.lightcrafts.utils.xml.XMLException;
 import com.lightcrafts.utils.xml.XmlDocument;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import javax.swing.JSeparator;
 import java.awt.Dimension;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
 /**
@@ -26,16 +33,13 @@ import java.util.prefs.Preferences;
 class OpTitle extends SelectableTitle {
 
     // Preferences are used to remember OpControl presets:
-    private final static Preferences Prefs =
-        Preferences.userNodeForPackage(OpTitle.class);
+    private final static Preferences Prefs = Preferences.userNodeForPackage(OpTitle.class);
     private final static String PresetsKey = "Presets";
 
-    private final static Dimension removeButtonSize = new Dimension(16, 16);
-
-    private OpControl control;
+    private final OpControl control;
+    private final OpTitleEditor editor;
     private JCheckBox activeCheckBox;
     private JButton removeButton;
-    private OpTitleEditor editor;
 
     OpTitle(final OpControl control, final OpStack stack) {
         super(control);
@@ -48,6 +52,10 @@ class OpTitle extends SelectableTitle {
         BufferedImage image = OpActions.getIcon(op);
         setIcon(image);
 
+        if (control.isRawCorrection()) {
+            return;
+        }
+
         activeCheckBox = new JCheckBox("", true);
         activeCheckBox.setToolTipText(LOCALE.get("DisableToolTip"));
         activeCheckBox.setRolloverEnabled(true);
@@ -58,7 +66,9 @@ class OpTitle extends SelectableTitle {
             activeCheckBox.setToolTipText(tip);
             control.setActivated(activeCheckBox.isSelected());
         });
+        buttonBox.add(activeCheckBox);
 
+        final Dimension removeButtonSize = activeCheckBox.getPreferredSize();
         removeButton = new JButton(new FlatInternalFrameCloseIcon());
         removeButton.setToolTipText(LOCALE.get("RemoveToolTip"));
         removeButton.setPreferredSize(removeButtonSize);
@@ -66,25 +76,18 @@ class OpTitle extends SelectableTitle {
         removeButton.setContentAreaFilled(false);
         removeButton.setBorder(null);
         removeButton.addActionListener(event -> stack.removeControl(control));
-
-        if (! control.isRawCorrection()) {
-            buttonBox.add(activeCheckBox);
-            // buttonBox.add(Box.createHorizontalStrut(ButtonSpace));
-            buttonBox.add(removeButton);
-            // buttonBox.add(Box.createHorizontalStrut(ButtonSpace));
-        }
-    }
-
-    OpControl getControl() {
-        return control;
+        buttonBox.add(removeButton);
     }
 
     void setActive(boolean active) {
-        // Won't generate an ActionEvent, so no risk of recursion with the
-        // activeButton ActionListener:
-        activeCheckBox.setSelected(active);
+        if (activeCheckBox != null) {
+            // Won't generate an ActionEvent, so no risk of recursion with the
+            // activeButton ActionListener:
+            activeCheckBox.setSelected(active);
+        }
     }
 
+    @Override
     void resetTitle(String title) {
         super.resetTitle(title);
         if (editor != null) {
@@ -93,162 +96,109 @@ class OpTitle extends SelectableTitle {
         }
     }
 
+    @Override
     JPopupMenu getPopupMenu() {
         JPopupMenu menu = super.getPopupMenu();
 
-        final OpStack stack = findOpStack();
-
-        boolean isLocked = control.isLocked();
+        final boolean isLocked = control.isLocked();
 
         if (! control.isRawCorrection()) {
-            if (! control.isActivated()) {
-                JMenuItem enableItem = new JMenuItem(
-                    LOCALE.get("EnableMenuItem")
-                );
-                enableItem.addActionListener(
-                    new ActionListener() {
-                        public void actionPerformed(ActionEvent event) {
-                            control.setActivated(true);
-                        }
-                    }
-                );
-                enableItem.setEnabled(! isLocked);
-                menu.add(enableItem, 0);
-            }
-            else {
-                JMenuItem disableItem = new JMenuItem(
-                    LOCALE.get("DisableMenuItem")
-                );
-                disableItem.addActionListener(
-                    new ActionListener() {
-                        public void actionPerformed(ActionEvent event) {
-                            control.setActivated(false);
-                        }
-                    }
-                );
-                disableItem.setEnabled(! isLocked);
-                menu.add(disableItem, 0);
-            }
+            final boolean isActivated = control.isActivated();
+
+            final String activateText = isActivated ?
+                LOCALE.get("DisableMenuItem") : LOCALE.get("EnableMenuItem");
+            final var activateItem = new JMenuItem(activateText);
+            activateItem.addActionListener(event -> control.setActivated(! isActivated));
+            activateItem.setEnabled(! isLocked);
+            menu.add(activateItem, 0);
+
             menu.add(new JSeparator(), 1);
         }
-        if (! isLocked) {
-            JMenuItem lockItem = new JMenuItem(
-                LOCALE.get("LockMenuItem")
-            );
-            lockItem.addActionListener(
-                new ActionListener() {
-                    public void actionPerformed(ActionEvent event) {
-                        control.setLocked(true);
-                    }
-                }
-            );
-            menu.add(lockItem, 2);
-        }
-        else {
-            JMenuItem unlockItem = new JMenuItem(
-                LOCALE.get("UnlockMenuItem")
-            );
-            unlockItem.addActionListener(
-                new ActionListener() {
-                    public void actionPerformed(ActionEvent event) {
-                        control.setLocked(false);
-                    }
-                }
-            );
-            menu.add(unlockItem, 2);
-        }
+
+        final String lockUnlockText = isLocked ?
+            LOCALE.get("UnlockMenuItem") : LOCALE.get("LockMenuItem");
+        final var lockUnlockItem = new JMenuItem(lockUnlockText);
+        lockUnlockItem.addActionListener(event -> control.setLocked(! isLocked));
+        menu.add(lockUnlockItem, 2);
+
         menu.add(new JSeparator(), 3);
 
-        JMenuItem savePreset = new JMenuItem(
-            LOCALE.get("RememberPresetMenuItem")
-        );
-        savePreset.addActionListener(
-            new ActionListener() {
-                public void actionPerformed(ActionEvent event) {
-                    XmlDocument preset = new XmlDocument("Preset");
-                    control.save(preset.getRoot());
-                    writePreset(preset);
-                }
-            }
-        );
-        menu.add(savePreset, 4);
+        final var savePresetItem = new JMenuItem(LOCALE.get("RememberPresetMenuItem"));
+        savePresetItem.addActionListener(event -> {
+            XmlDocument preset = new XmlDocument("Preset");
+            control.save(preset.getRoot());
+            writePreset(preset);
+        });
+        menu.add(savePresetItem, 4);
 
-        JMenuItem applyPreset = new JMenuItem(
-            LOCALE.get("ApplyPresetMenuItem")
-        );
+        final var applyPresetItem = new JMenuItem(LOCALE.get("ApplyPresetMenuItem"));
         final XmlDocument preset = readPreset();
-        applyPreset.addActionListener(
-            new ActionListener() {
-                public void actionPerformed(ActionEvent event) {
-                    try {
-                        // Like control.restore(), but with undo:
-                        control.restorePresets(preset.getRoot());
-                    }
-                    catch (XMLException e) {
-                        // No way to back out of this, so leave things as
-                        // they are and hope the user can recover.
-                        System.err.println(
-                            "Error in preset restore: " + e.getMessage()
-                        );
-                    }
-                }
-            }
-        );
         if ((preset == null) || isLocked) {
-            applyPreset.setEnabled(false);
+            applyPresetItem.setEnabled(false);
         }
-        menu.add(applyPreset, 5);
+        else {
+            applyPresetItem.addActionListener(event -> {
+                try {
+                    // Like control.restore(), but with undo:
+                    control.restorePresets(preset.getRoot());
+                }
+                catch (XMLException e) {
+                    // No way to back out of this, so leave things as
+                    // they are and hope the user can recover.
+                    System.err.println("Error in preset restore: " + e.getMessage());
+                }
+            });
+        }
+        menu.add(applyPresetItem, 5);
 
         if (! control.isRawCorrection()) {
-            JMenuItem deleteItem = new JMenuItem(LOCALE.get("DeleteMenuItem"));
-            deleteItem.addActionListener(
-                new ActionListener() {
-                    public void actionPerformed(ActionEvent event) {
-                        stack.removeControl(control);
-                    }
-                }
-            );
+            final OpStack stack = findOpStack();
+
+            final var deleteItem = new JMenuItem(LOCALE.get("DeleteMenuItem"));
+            deleteItem.addActionListener(event -> stack.removeControl(control));
             deleteItem.setEnabled(! isLocked);
 
             menu.add(new JSeparator());
-
             menu.add(deleteItem);
         }
         return menu;
     }
 
-    // Read in the preset for the current OpControl type from Preferences,
-    // or null if no such preset exists.
-    private XmlDocument readPreset() {
+    /**
+     * Read in the preset for the current OpControl type from Preferences,
+     * or null if no such preset exists.
+     */
+    private @Nullable XmlDocument readPreset() {
         String key = getPresetsKey();
         String text = Prefs.get(key, "");
         try {
-            ByteArrayInputStream in =
-                new ByteArrayInputStream(text.getBytes("UTF-8"));
+            var in = new ByteArrayInputStream(text.getBytes(StandardCharsets.UTF_8));
             return new XmlDocument(in);
         }
-        catch (Exception e) {   // IOException or XMLException
+        catch (IOException e) {   // IOException or XMLException
             return null;
         }
     }
 
-    // Save the given preset in Preferences for the current OpControl type.
-    private void writePreset(XmlDocument preset) {
+    /**
+     * Save the given preset in Preferences for the current OpControl type.
+     */
+    private void writePreset(@NotNull XmlDocument preset) {
         String key = getPresetsKey();
         try {
             ByteArrayOutputStream out = new ByteArrayOutputStream();
             preset.write(out);
-            String text = out.toString("UTF-8");
+            String text = out.toString(StandardCharsets.UTF_8);
             Prefs.put(key, text);
             Prefs.sync();
         }
-        catch (Exception e) {   // IOException or BackingStoreException
+        catch (IOException | BackingStoreException e) {
             // Make sure the "Apply Preset" item doesn't get enabled:
             Prefs.remove(key);
         }
     }
 
-    private String getPresetsKey() {
+    private @NotNull String getPresetsKey() {
         String name = control.getOperation().getType().getName();
         return PresetsKey + name;
     }
