33ca6d1c09f4b5b87e0dbc8ce840ab2d28e41f70
Masahiro Kitagawa <arctica0316@gmail.com>
2025-03-17 17:17:07 +0900

    build(linux): Create debian package with jpackage

--- linux/build.gradle.kts
+++ linux/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm")
     id("lightzone.java-conventions")
+    id("org.beryx.runtime")
 }
 dependencies {
     implementation(project(":lightcrafts"))
@@ -8,4 +9,54 @@ dependencies {
 }
 application {
     mainClass.set("com.lightcrafts.platform.linux.LinuxLauncher")
+}
+tasks {
+    named("build") {
+        dependsOn(":lightcrafts:build")
+    }
+    named("clean") {
+        dependsOn(":lightcrafts:clean")
+    }
+    register<Exec> ("helpFiles") {
+        commandLine("ant", "-f", "help/build.xml")
+    }
+    named("jpackage") {
+        dependsOn("build", "helpFiles")
+        doFirst {
+            copy {
+                from("products/")
+                include("dcraw_lz")
+                include("LightZone-forkd")
+                into(layout.buildDirectory.dir("jpackage/lightzone/bin"))
+            }
+            copy {
+                from("products/")
+                include("*.so")
+                into(layout.buildDirectory.dir("jpackage/lightzone/lib/app"))
+            }
+        }
+    }
+}
+runtime {
+    options.set(listOf("--strip-debug", "--no-header-files", "--no-man-pages"))
+    modules.set(
+        listOf(
+            "java.base", "java.desktop", "java.logging", "java.management", "java.prefs", "java.rmi", "jdk.management"
+        )
+    )
+    jpackage {
+        imageName = "lightzone"
+        installerName = "lightzone"
+        skipInstaller = false
+        installerOptions = listOf("--vendor", "LightZone Project")
+        imageOptions = listOf("--icon", "icons/hicolor/256x256/apps/lightzone.png")
+        installerType = "deb"
+    }
+    launcher {
+        jvmArgs = listOf(
+            "--add-exports=java.desktop/sun.awt.image=ALL-UNNAMED",
+            "-Djava.library.path=\$APPDIR"
+        )
+        runInBinDir = true
+    }
 }
