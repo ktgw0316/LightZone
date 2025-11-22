137c50a5aab1f4ec2ce3c03f51439f5c354e2e5c
Masahiro Kitagawa <arctica0316@gmail.com>
2025-11-22 15:29:50 +0900

    fix(mac): CMProfileLocation is deprecated
    - Remove code for 32-bit and unsupported OS version

--- macosx/jnisrc/libMacOSX/ColorProfileManager.mm
+++ macosx/jnisrc/libMacOSX/ColorProfileManager.mm
@@ -18,51 +18,6 @@
 using namespace std;
 using namespace LightCrafts;
 
-////////// Local functions ////////////////////////////////////////////////////
-
-#ifndef __LP64__
-/**
- * Get the full path of a file (as a UTF-8 string) from an FSSpec.
- */
-static bool getPathFromFSSpec( FSSpec const *pFSSpec, char *buf, int bufSize ) {
-    FSRef fsRef;
-    FSpMakeFSRef( pFSSpec, &fsRef );
-
-    CFURLRef cfURLRef = CFURLCreateFromFSRef( NULL, &fsRef );
-    if ( !cfURLRef )
-        return false;
-
-    CFStringRef cfPath =
-        CFURLCopyFileSystemPath( cfURLRef, kCFURLPOSIXPathStyle );
-
-    bool const result =
-        CFStringGetCString( cfPath, buf, bufSize, kCFStringEncodingUTF8 );
-
-    CFRelease( cfPath );
-    CFRelease( cfURLRef );
-    return result;
-}
-#endif /* __LP64__ */
-
-/**
- * Get a color profile's path from a CMProfileLocation.  Note that is the
- * caller's responsibility to delete the string returned.
- */
-static char* getProfilePath( CMProfileLocation const &loc ) {
-    switch ( loc.locType ) {
-#ifndef __LP64__
-        case cmFileBasedProfile:
-            char path[ PATH_MAX ];
-            if ( getPathFromFSSpec( &loc.u.fileLoc.spec, path, sizeof path ) )
-                return new_strdup( path );
-            break;
-#endif
-        case cmPathBasedProfile:
-            return new_strdup( loc.u.pathLoc.path );
-    }
-    return NULL;
-}
-
 ////////// JNI ////////////////////////////////////////////////////////////////
 
 #define MacOSXColorProfileManager_METHOD(method) \
@@ -75,7 +30,6 @@ JNIEXPORT jstring JNICALL
 MacOSXColorProfileManager_METHOD(getSystemDisplayProfilePath)
     ( JNIEnv *env, jclass )
 {
-#ifdef MAC_OS_X_VERSION_10_11
     CGDirectDisplayID displayID = CGMainDisplayID();
     CFUUIDRef displayUUID = CGDisplayCreateUUIDFromDisplayID(displayID);
     if (!displayUUID) {
@@ -158,24 +112,6 @@ MacOSXColorProfileManager_METHOD(getSystemDisplayProfilePath)
         return NULL;
     }
     char const *cPath = new_strdup( path );
-#else // for Snow Leopard or older
-    CMError err;
-    CMProfileRef profRef;
-    CMProfileLocation profLoc;
-    UInt32 locSize = cmCurrentProfileLocationSize;
-
-    CGDirectDisplayID const displayID = CGMainDisplayID();
-    err = CMGetProfileByAVID( (CMDisplayIDType)displayID, &profRef );
-    if ( err != noErr )
-        return NULL;
-    err = NCMGetProfileLocation( profRef, &profLoc, &locSize );
-    if ( err != noErr )
-        return NULL;
-    char const *const cPath = getProfilePath( profLoc );
-    CMCloseProfile( profRef );
-    if ( !cPath )
-        return NULL;
-#endif
     jstring const jPath = env->NewStringUTF( cPath );
     delete[] cPath;
     return jPath;
