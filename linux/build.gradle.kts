33bc26e733012e79887a23682325bb9e791a4271
Masahiro Kitagawa <arctica0316@gmail.com>
2026-05-03 14:50:44 +0900

    build(gradle): Exclude unused jars from packages

--- linux/build.gradle.kts
+++ linux/build.gradle.kts
@@ -46,6 +46,26 @@ tasks {
                 include("**")
                 into(layout.buildDirectory.dir("jpackage/lightzone/share/icons"))
             }
+
+            val jarsToDelete = listOf(
+                "annotations*.jar",
+                "antlr4-runtime*.jar",
+                "bigint*.jar",
+                "checker-qual*.jar",
+                "commons-compiler*.jar",
+                "error_prone_annotations*.jar",
+                "failureaccess*.jar",
+                "guava*.jar",
+                "j2objc-annotations*.jar",
+                "janino*.jar",
+                "jsr305*.jar",
+                "jts-core*.jar",
+                "kotlin-stdlib*.jar",
+                "listenablefuture*.jar"
+            )
+            jarsToDelete.forEach { jar ->
+                delete(fileTree(layout.buildDirectory.dir("jpackage/lightzone/lib/app")).matching { include(jar) })
+            }
         }
     }
 }
--- macosx/build.gradle.kts
+++ macosx/build.gradle.kts
@@ -40,6 +40,26 @@ tasks {
                 include("*.icns")
                 into(layout.buildDirectory.dir("jpackage/LightZone.app/Contents/Resources"))
             }
+
+            val jarsToDelete = listOf(
+                "annotations*.jar",
+                "antlr4-runtime*.jar",
+                "bigint*.jar",
+                "checker-qual*.jar",
+                "commons-compiler*.jar",
+                "error_prone_annotations*.jar",
+                "failureaccess*.jar",
+                "guava*.jar",
+                "j2objc-annotations*.jar",
+                "janino*.jar",
+                "jsr305*.jar",
+                "jts-core*.jar",
+                "kotlin-stdlib*.jar",
+                "listenablefuture*.jar"
+            )
+            jarsToDelete.forEach { jar ->
+                delete(fileTree(layout.buildDirectory.dir("jpackage/lightzone/lib/app")).matching { include(jar) })
+            }
         }
         doLast {
             val versionSuffix = "-${project.version.toString().substringBefore("b")}.dmg"
