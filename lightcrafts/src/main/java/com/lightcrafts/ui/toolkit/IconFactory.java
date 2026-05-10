d66ea884789847edf88e52ab1af8a23d7bfb1411
Masahiro Kitagawa <arctica0316@gmail.com>
2026-05-10 23:05:06 +0900

    refactor: IconFactory

--- lightcrafts/src/main/java/com/lightcrafts/ui/toolkit/IconFactory.java
+++ lightcrafts/src/main/java/com/lightcrafts/ui/toolkit/IconFactory.java
@@ -2,8 +2,6 @@
 
 package com.lightcrafts.ui.toolkit;
 
-import org.eclipse.imagen.ImageN;
-import org.eclipse.imagen.PlanarImage;
 import org.eclipse.imagen.RenderedOp;
 import org.eclipse.imagen.media.algebra.AlgebraDescriptor;
 import org.eclipse.imagen.media.bandmerge.BandMergeDescriptor;
@@ -17,10 +15,9 @@ import java.awt.*;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
-import java.awt.image.renderable.ParameterBlock;
-import java.awt.image.renderable.RenderableImage;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 
 import static org.eclipse.imagen.media.algebra.AlgebraDescriptor.Operator.INVERT;
 
@@ -40,46 +37,40 @@ public class IconFactory {
      * object (like a button that uses the icon) with an extra "resources"
      * name at the end.
      */
-    public static Icon createIcon(Class clazz, String name) {
+    public static Icon createIcon(Class<?> clazz, String name) {
         return createIcon(clazz, name, StandardSize);
     }
 
-    public static Icon createIcon(Class clazz, String name, int size) {
+    public static Icon createIcon(Class<?> clazz, String name, int size) {
         try {
             URL url = clazz.getResource("resources/" + name);
-            BufferedImage image = ImageIO.read(url);
+            BufferedImage image = ImageIO.read(Objects.requireNonNull(url));
             image = getScaledImage(image, size);
             return new ImageIcon(image);
         }
-        catch (Throwable t) {   // IOException, null URL
-            logger.error("Couldn't read icon resource \"{}\" for class {}", name, clazz.getName(), t);
+        catch (IOException | NullPointerException e) {   // IOException, null URL
+            logger.error("Couldn't read icon resource \"{}\" for class {}", name, clazz.getName(), e);
             throw new RuntimeException(
-                "Couldn't read icon resource \"" + name + "\" " +
-                "for class " + clazz.getName()
-            );
+                    "Couldn't read icon resource \"" + name + "\" " + "for class " + clazz.getName());
         }
     }
 
-    public static Icon createInvertedIcon(Class clazz, String name) {
+    public static Icon createInvertedIcon(Class<?> clazz, String name) {
         return createInvertedIcon(clazz, name, StandardSize);
     }
 
-    public static Icon createInvertedIcon(Class clazz, String name, int size) {
+    public static Icon createInvertedIcon(Class<?> clazz, String name, int size) {
         Icon icon = createIcon(clazz, name, size);
-        if (icon != null) {
-            icon = invertIcon(icon);
-        }
-        return icon;
+        return invertIcon(icon);
     }
 
     public static Icon invertIcon(Icon icon) {
         if (icon == null)
             return null;
-        
-        int width = icon.getIconWidth();
-        int height = icon.getIconHeight();
-        BufferedImage image =
-            new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
+
+        final int width = icon.getIconWidth();
+        final int height = icon.getIconHeight();
+        var image = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
         Graphics g = image.createGraphics();
         icon.paintIcon(null, g, 0, 0);
         image = invertImageWithAlpha(image);
@@ -91,38 +82,23 @@ public class IconFactory {
     // which appears to be true for BufferedImages read from resources by
     // ImageIO.read() and constructed above in invertIcon().
     private static BufferedImage invertImageWithAlpha(BufferedImage image) {
-        RenderedOp alpha =
-            BandSelectDescriptor.create(image, new int[] { 3 }, null);
-        RenderedOp colors =
-            BandSelectDescriptor.create(image, new int[] { 0, 1, 2 }, null);
+        RenderedOp alpha = BandSelectDescriptor.create(image, new int[] { 3 }, null);
+        RenderedOp colors = BandSelectDescriptor.create(image, new int[] { 0, 1, 2 }, null);
         colors = AlgebraDescriptor.create(INVERT, null, null, 0, null, colors);
         RenderedOp op = BandMergeDescriptor.create(null, 0, true, null, colors, alpha);
         return op.getAsBufferedImage();
     }
-    
+
     public static BufferedImage getScaledImage(BufferedImage image, int limit) {
         int maxSize = Math.max(image.getWidth(), image.getHeight());
         if (maxSize > limit) {
-            if (true) {
-                final double scale = limit / (float) maxSize;
-                final AffineTransform transform =
-                        AffineTransform.getScaleInstance(scale, scale);
-                final int interp = AffineTransformOp.TYPE_BILINEAR;
-                final AffineTransformOp op = new AffineTransformOp(transform, interp);
-                final BufferedImage rescaled = op.createCompatibleDestImage(image, null);
-                op.filter(image, rescaled);
-                image = rescaled;
-            } else {
-                BufferedImage rescaled = new BufferedImage(limit, limit, BufferedImage.TYPE_4BYTE_ABGR);
-                Graphics2D g = (Graphics2D) rescaled.getGraphics();
-                double scale = limit / (float) maxSize;
-                AffineTransform transform =
-                        AffineTransform.getScaleInstance(scale, scale);
-                // g.setTransform(transform);
-                g.drawRenderedImage(image, transform);
-                g.dispose();
-                image = rescaled;
-            }
+            final double scale = limit / (double) maxSize;
+            final var transform = AffineTransform.getScaleInstance(scale, scale);
+            final int interp = AffineTransformOp.TYPE_BILINEAR;
+            final AffineTransformOp op = new AffineTransformOp(transform, interp);
+            final BufferedImage rescaled = op.createCompatibleDestImage(image, null);
+            op.filter(image, rescaled);
+            image = rescaled;
         }
         return image;
     }
